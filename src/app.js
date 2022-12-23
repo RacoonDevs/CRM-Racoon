673fdf51af2079e3c058bc16ac0c07b73165fe2d
Raul Belloso M <raul.belloso.m@gmail.com>
2022-12-22 22:38:48 -0600

    try to build in prod

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.routes.js";
 import pingRoutes from "./routes/ping.routes.js";
 import usersRoutes from "./routes/users.routes.js";
@@ -7,6 +9,8 @@ import teamRoutes from "./routes/teams.routes.js";
 import { verifyToken } from "./middlewares/index.js";
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 app.use(cors());
 
 app.use(express.json());
@@ -15,6 +19,8 @@ app.use("/api", pingRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/user", verifyToken, usersRoutes);
 app.use("/api/team", verifyToken, teamRoutes);
+
+app.use(express.static(join(__dirname, "../front/build")));
 
 app.use((req, res, next) => {
   res.status(404).json({
